ea9c311cbd57d036728730393876b0e7292d3517
mace <masonbunyan@hotmail.com>
2023-12-07 21:42:46 +0000

    Add database settings & write wait_for_db functionality.

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,19 +1,21 @@
 
-from django.test import SimpleTestCase 
-from app import calc 
+from django.test import SimpleTestCase
+from app import calc
+
 
 class CalcTests(SimpleTestCase):
     """ 
     Test calc module
-    
+
     """
+
     def test_add_numbers(self):
         """ 
         Test adding numbers together
-        
+
         """
         res = calc.add(5, 6)
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_sub_numbers(self):
         res = calc.subtract(7, 2)
