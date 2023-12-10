3b7acdf2a5a03ad618681c5f9a2670d66d145c41
mace <masonbunyan@hotmail.com>
2023-12-10 17:46:58 +0000

    Add the user models & edit the admin page to be able to create new users.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,54 @@
-from django.contrib import admin
+"""
+Django admin custom
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext_lazy as _
+
+class UserAdmin(BaseUserAdmin):
+    """
+    Define admin pages for user
+    """
+    ordering = ["id"]
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields' : ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+
+        ),
+
+        (
+            _('Important Dates'),
+        {
+            'fields': ('last_login',)
+        }
+        )
+    )
+
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
